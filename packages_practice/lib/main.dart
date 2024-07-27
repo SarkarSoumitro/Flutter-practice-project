cac27dfbe0bafc08d3f3901da8d18eff4de436b0
SarkarSoumitro <soumitrosarkar78@gmail.com>
2024-07-28 00:19:43 +0600

    g

---
+++ packages_practice/lib/main.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:packages_practice/Packages/Liquid_pull_request.dart';
+import 'package:packages_practice/Packages/goggle_fonts.dart';
+import 'package:packages_practice/Packages/percentage_indicator.dart';
+
+import 'Packages/avater_glow.dart';
+
+void main(){
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+     // home: Glow(),
+      //home: Fonts(),
+        //home: Liquid_pull(),
+      home: percentage(),
+     );
+  }
+}
