93dc998ed7eec8082fa0033cec27dcd4458d1f44
SarkarSoumitro <soumitrosarkar78@gmail.com>
2024-07-28 23:50:45 +0600

    cube updated

---
+++ packages_practice/lib/Packages/cube_swipe.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class cube extends StatelessWidget {
+  const cube({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
