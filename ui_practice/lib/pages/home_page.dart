af31dba9da976a86f0dba3f2924fcea7c235703a
SarkarSoumitro <soumitrosarkar78@gmail.com>
2024-08-01 23:51:35 +0600

    ui page updated

--- ui_practice/lib/pages/home_page.dart
+++ ui_practice/lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
 class intropage extends StatelessWidget {
   const intropage({super.key});
 
@@ -11,24 +12,36 @@ class intropage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepOrangeAccent[200],
-      body: Padding(
-        padding: const EdgeInsets.all(50),
-        child: Column(
-          children: [
-            SizedBox(height: 25),
-            ///shop name
-            Text("SHUSHI MAN",
-              style: GoogleFonts.dmSerifDisplay(fontSize: 28,color: Colors.white),),
+      body:SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            children: [
+              SizedBox(height: 25),
+              ///shop name
+              Text("SHUSHI MAN",
+                style: GoogleFonts.dmSerifDisplay(fontSize: 28,color: Colors.white),),
 
-            //icon
-            const SizedBox(height: 25,),
-            //title
+              //icon
+              const SizedBox(height: 25,),
+              Image.asset("images/sushi.png"),
+              //title
+              Text("THE TASTE OF JAPANES FOOD",
+                style:GoogleFonts.dmSerifDisplay(
+                    fontSize: 44,
+                    color: Colors.white),),
+              const SizedBox(height: 10,),
 
-            //subtitle
+              //subtitle
+               Text("Feel the taste of the most popular japanese foood from anywhere and anytime",
+               style: TextStyle(
+                 color: Colors.grey[300],
+                 height: 2
+               ),)
+              //button
 
-            //button
-
-          ],
+            ],
+          ),
         ),
       ),
     );
