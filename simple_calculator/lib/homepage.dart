1f950b4115435ce669a7a7e9bce49782e5f642e4
SarkarSoumitro <soumitrosarkar78@gmail.com>
2024-08-30 20:25:16 +0600

    complete

--- simple_calculator/lib/homepage.dart
+++ simple_calculator/lib/homepage.dart
@@ -8,6 +8,7 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  double _result = 0;
   final TextEditingController _firstnumbercontroller = TextEditingController();
   final TextEditingController _secondnumbercontroller = TextEditingController();
 
@@ -33,13 +34,102 @@ class _HomepageState extends State<Homepage> {
               height: 20,
             ),
             TextField(
-              controller: _firstnumbercontroller,
+              controller: _secondnumbercontroller,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: "Enter a number"),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: _ontabaddbutton,
+                    icon: Icon(Icons.add, size: 40)),
+                IconButton(
+                    onPressed: _ontabsubtractbutton,
+                    icon: Icon(
+                      Icons.remove,
+                      size: 40,
+                    )),
+                TextButton(
+                    onPressed: _ontabdevidebutton,
+                    child: Text(
+                      "/",
+                      style: TextStyle(fontSize: 40, color: Colors.black),
+                    )),
+                TextButton(
+                    onPressed: _ontabmultiplybutton,
+                    child: Text(
+                      "*",
+                      style: TextStyle(fontSize: 40, color: Colors.black),
+                    )),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Text(
+              "Result: $_result",
+              style: TextStyle(
+                fontSize: 20,
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  void _ontabaddbutton() {
+    if (_validTextfield() == false) {
+      return;
+    }
+    double firstnumb = double.tryParse(_firstnumbercontroller.text) ?? 0;
+    double secondnumb = double.tryParse(_secondnumbercontroller.text) ?? 0;
+    _result = firstnumb + secondnumb;
+    setState(() {});
+  }
+
+  void _ontabsubtractbutton() {
+    if (_validTextfield() == false) {
+      return;
+    }
+
+    double firstnumb = double.tryParse(_firstnumbercontroller.text) ?? 0;
+    double secondnumb = double.tryParse(_secondnumbercontroller.text) ?? 0;
+    _result = firstnumb - secondnumb;
+    setState(() {});
+  }
+
+  void _ontabdevidebutton() {
+    if (_validTextfield() == false) {
+      return;
+    }
+    double firstnumb = double.tryParse(_firstnumbercontroller.text) ?? 0;
+    double secondnumb = double.tryParse(_secondnumbercontroller.text) ?? 0;
+    _result = firstnumb / secondnumb;
+    setState(() {});
+  }
+
+  void _ontabmultiplybutton() {
+    if (_validTextfield() == false) {
+      return;
+    }
+    double firstnumb = double.tryParse(_firstnumbercontroller.text) ?? 0;
+    double secondnumb = double.tryParse(_secondnumbercontroller.text) ?? 0;
+    _result = firstnumb * secondnumb;
+    setState(() {});
+  }
+
+  bool _validTextfield() {
+    if (_firstnumbercontroller.text.isEmpty) {
+      return false;
+    } else if (_secondnumbercontroller.text.isEmpty) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
