416a24efe5884f3d9c7f768d0010cdd5377ff034
SarkarSoumitro <soumitrosarkar78@gmail.com>
2024-11-03 23:48:38 +0600

    updated

--- module_14_task_management/lib/component/TaskList.dart
+++ module_14_task_management/lib/component/TaskList.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:module_14_task_management/style/style.dart';
 
 ListView TaskList(TaskItems) {
   return ListView.builder(
       itemCount: TaskItems.length,
       itemBuilder: (context, index) {
-        return Card();
+        return Card(
+            child: ItemSizeBox(Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              TaskItems[index]['title'],
+              style: Head6Text(colorDarkBlue),
+            ),
+            Text(
+              TaskItems[index]['description'],
+              style: Head7Text(colorDarkBlue),
+            ),
+          ],
+        )));
       });
 }
